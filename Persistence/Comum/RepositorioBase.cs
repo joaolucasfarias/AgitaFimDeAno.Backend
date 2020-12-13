8e36fc5df9cee2e8b1e853c335aa6d1da990a8fe
João Lucas Farias <joao@sistemasbr.net>
2020-12-13 12:50:12 -0300

    Criando e implementando outras interfaces. Dessa vez para a persistência

---
+++ Persistence/Comum/RepositorioBase.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Domain.Comum;
+using Domain.Filmes;
+using System.Collections.Generic;
+
+namespace Persistence.Comum
+{
+    public abstract class RepositorioBase<T> : IRepositorio<T> where T : AggregateRoot
+    {
+        protected IList<Filme> Filmes;
+
+        protected RepositorioBase() =>
+            Filmes ??= new List<Filme>();
+
+        public abstract bool Adicionar(T objeto, out string mensagem);
+
+        public abstract bool Editar(T objet, out string mensagem);
+
+        public abstract bool Excluir(int id, out string mensagem);
+
+        public abstract IEnumerable<T> Listar();
+
+        public abstract T Carregar(int id);
+    }
+}
---
+++ Persistence/Filmes/FilmesRepositorio.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interfaces;
+using Domain.Filmes;
+using Persistence.Comum;
+using System;
+using System.Collections.Generic;
+
+namespace Persistence.Filmes
+{
+    public sealed class FilmesRepositorio : RepositorioBase<Filme>, IFilmesRepositorio
+    {
+        public override bool Adicionar(Filme objeto, out string mensagem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Editar(Filme objet, out string mensagem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Excluir(int id, out string mensagem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Filme> Listar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Filme Carregar(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
