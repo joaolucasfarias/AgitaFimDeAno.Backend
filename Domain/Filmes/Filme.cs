40f7969baa541073e7c9d199ff3262c7c390ba53
João Lucas Farias <joao@sistemasbr.net>
2020-12-13 12:05:47 -0300

    Finalizando primeiro esboço da entidade

--- Domain/Filmes/Filme.cs
+++ Domain/Filmes/Filme.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Comum;
+﻿using System;
+using Domain.Comum;
 using Domain.Filmes.ValueObjects;
 
 namespace Domain.Filmes
@@ -8,8 +9,25 @@ namespace Domain.Filmes
         public Nome Nome { get; }
         public Descricao Descricao { get; }
 
-        public Filme(int id) : base(id)
+        private Filme(int id) : base(id)
         {
+        }
+
+        private Filme(Nome nome, Descricao descricao) : this(0)
+        {
+            Nome = nome;
+            Descricao = descricao;
+        }
+
+        public static Filme Novo(Nome nome, Descricao descricao)
+        {
+            if (nome is null)
+                throw new ArgumentNullException(nameof(nome));
+
+            if (descricao is null)
+                throw new ArgumentNullException(nameof(nome));
+
+            return new Filme(nome, descricao);
         }
     }
 }
