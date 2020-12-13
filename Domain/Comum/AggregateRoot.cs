92a1652098633a824601209f8534cde4e687f85e
João Lucas Farias <joao@sistemasbr.net>
2020-12-13 11:21:21 -0300

    Classes comundo em todos os domínios

---
+++ Domain/Comum/AggregateRoot.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Comum
+{
+    public abstract class AggregateRoot : Entity
+    {
+        protected AggregateRoot(int id) : base(id) { }
+    }
+}
---
+++ Domain/Comum/Entity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Domain.Comum
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public int Id { get; protected set; }
+
+        protected Entity(int id) =>
+            Id = id;
+
+        public override bool Equals(object outroObjeto)
+        {
+            if (!(outroObjeto is Entity entity))
+                return false;
+
+            return Equals(entity);
+        }
+
+        public override int GetHashCode() =>
+            Id.GetHashCode();
+
+        public bool Equals(Entity outro) =>
+            outro != null && !(Id == 0 || outro.Id == 0) && Id.Equals(outro.Id);
+
+        public static bool operator ==(Entity x, Entity y) =>
+            x.Equals(y);
+
+        public static bool operator !=(Entity x, Entity y) =>
+            !(x == y);
+    }
+}
