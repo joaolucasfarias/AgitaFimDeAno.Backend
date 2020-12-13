a9cfeca8306e12c9856afb5edcc26100c6c6d515
João Lucas Farias <joao@sistemasbr.net>
2020-12-13 12:57:14 -0300

    Adicionando injeção de dependência

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Filmes.Comandos.NovoFilme;
+using Application.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Persistence.Filmes;
 
 namespace WebApi
 {
@@ -25,6 +21,9 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<INovoFilmeComando, NovoFilmeComando>();
+            services.AddSingleton<IFilmesRepositorio, FilmesRepositorio>();
+
             services.AddControllers();
         }
 
